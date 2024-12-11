f710062d10ba7da5361fb1b509fe94680d815638
Filip Bäck <filip.back@combitech.com>
2024-12-11 10:26:39 +0100

    day9 part 1 successful. cba doing part 2

--- 9/day9.py
+++ 9/day9.py
@@ -1,61 +1,79 @@
 
-def read_input():
-    with open("input.txt", "r") as file:
-        input_string = file.read()
-    return input_string
-
-def parse():
-    data = read_input().replace("\n", "")  # Remove newline characters
-    new_string = ""
-
+def parse(data):
+    result = []
     index = 0
-    for i, char in enumerate(data):
-        value = int(char)  # Convert the character to an integer
-        if i % 2 == 0:
-            new_string += str(index) * value
-            index += 1
-        else:
-            new_string += "." * value  # Repeat the dot
+    i = 0
 
-    return new_string
+    while i < len(data):
+        # even
+        value1 = int(data[i])
+        result.append([index] * value1)
+        i += 1
 
-def move_blocks():
-    disk_map = parse()
-    disk_map = list(disk_map)  
+        # odd 
+        if i < len(data):
+            value2 = int(data[i])
+            if value2 > 0:
+                result.append([-1] * value2)  # -1 as dots
+            i += 1
 
-    while True:
-        # Find the left-most free space
-        try:
-            leftmost_dot_index = disk_map.index('.')
-        except ValueError:
-            leftmost_dot_index = len(disk_map)
+        index += 1
 
-        # Find the right-most block of numbers
-        rightmost_index = len(disk_map) - 1
-        while rightmost_index > 0 and disk_map[rightmost_index] == '.':
+    return result
+
+def move_blocks(parsed_data):
+
+    
+    disk_map = [item for sublist in parsed_data for item in sublist]
+    
+    leftmost_dot_index = 0
+    rightmost_index = len(disk_map) - 1
+
+    while leftmost_dot_index < rightmost_index:
+
+        # find left-most dot
+        while leftmost_dot_index < len(disk_map) and disk_map[leftmost_dot_index] != -1:
+            leftmost_dot_index += 1
+
+        # find the right-most number
+        while rightmost_index >= 0 and disk_map[rightmost_index] == -1:
             rightmost_index -= 1
 
-        # If there are no more blocks to move, break the loop
-        if leftmost_dot_index >= rightmost_index:
-            break
+        # swap if good
+        if leftmost_dot_index < rightmost_index:
+            disk_map[leftmost_dot_index], disk_map[rightmost_index] = disk_map[rightmost_index], -1
 
-        # Move one block from the right-most sequence to the left-most dot
-        disk_map[leftmost_dot_index] = disk_map[rightmost_index]
-        disk_map[rightmost_index] = '.'
+            # Move indices inward
+            leftmost_dot_index += 1
+            rightmost_index -= 1
+        else:
+            break  
 
-    return ''.join(disk_map)
+    return disk_map
 
-def calculate_sum():
-    result_string = move_blocks()
+def calculate_sum(result_list):
     total_sum = 0
-    for i, char in enumerate(result_string):
-        if char.isdigit():
-            total_sum += i * int(char)
-        elif char == '.':
-            break  # Stop summing once a dot is encountered
+    for i, value in enumerate(result_list):
+        if value == -1:
+            break
+        else:
+            total_sum += i * value
     return total_sum
 
-# Calculate the sum and write it to output2.txt
-result_sum = calculate_sum()
-with open("output2.txt", "w") as file:
-    file.write(str(result_sum))
+
+def main():
+    with open("input.txt", "r") as file:
+        input_string = file.read().strip()
+    parsed_data = parse(input_string)
+    print("Parsed Data:", parsed_data)
+    moved_data = move_blocks(parsed_data)  
+    print("Moved Data:", moved_data)
+    result_sum = calculate_sum(moved_data)
+    print("Resulting Sum:", result_sum)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
